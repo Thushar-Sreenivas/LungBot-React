6a428cca7bdc7611ac424178dd6bcb85d181a215
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-06-17 20:20:23 +0530

    Updates

---
+++ precache-manifest.5403d46f7aece130630346de46b9455d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3780509ae8273e4505ba77cc4f66f5f9",
+    "url": "/LungBot-React/index.html"
+  },
+  {
+    "revision": "713b20aedcbe1a75e778",
+    "url": "/LungBot-React/static/css/main.8c6f7d52.chunk.css"
+  },
+  {
+    "revision": "9961a5af69535685df34",
+    "url": "/LungBot-React/static/js/2.84ec4349.chunk.js"
+  },
+  {
+    "revision": "1bbf3207ff32fc1e12190f0b21fa7e98",
+    "url": "/LungBot-React/static/js/2.84ec4349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713b20aedcbe1a75e778",
+    "url": "/LungBot-React/static/js/main.269fffc1.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/LungBot-React/static/js/main.269fffc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6add208907cb8511915",
+    "url": "/LungBot-React/static/js/runtime-main.d2b72f1d.js"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/LungBot-React/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/LungBot-React/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/LungBot-React/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "2e2571256140872fb7e4f246ea786669",
+    "url": "/LungBot-React/static/media/github.2e257125.svg"
+  },
+  {
+    "revision": "08ea438ed3b14e63dd54516d3768e3d3",
+    "url": "/LungBot-React/static/media/google.08ea438e.svg"
+  },
+  {
+    "revision": "ddaf636676f8b04cdec8cdb40750277f",
+    "url": "/LungBot-React/static/media/james.ddaf6366.jpg"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/LungBot-React/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "96764d80118b4ece734dc14401283b76",
+    "url": "/LungBot-React/static/media/lora.96764d80.jpg"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/LungBot-React/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "be4a1748d0f94c35e0a661164495d160",
+    "url": "/LungBot-React/static/media/mike.be4a1748.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/LungBot-React/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/LungBot-React/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/LungBot-React/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/LungBot-React/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/LungBot-React/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/LungBot-React/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/LungBot-React/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/LungBot-React/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "2515c8e5bccbb8334071a0cce1bb2540",
+    "url": "/LungBot-React/static/media/profile-page.2515c8e5.png"
+  },
+  {
+    "revision": "9d4a558e7169ed763a05715627bc088c",
+    "url": "/LungBot-React/static/media/ryan.9d4a558e.jpg"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/LungBot-React/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/LungBot-React/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/LungBot-React/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/LungBot-React/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/LungBot-React/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/LungBot-React/static/media/square5.ff004a0e.png"
+  }
+]);
